904b1a8368957ec239b5d2b7c42c1022631e27c2
dgcgary <d.garcia.1@estudiantec.cr>
2024-11-14 18:34:11 -0600

    Timer añadido

--- AirWar/Form1.Designer.cs
+++ AirWar/Form1.Designer.cs
@@ -2,15 +2,10 @@
 {
     partial class Form1
     {
-        /// <summary>
-        ///  Required designer variable.
-        /// </summary>
         private System.ComponentModel.IContainer components = null;
+        private System.Windows.Forms.Timer timer;
+        private System.Windows.Forms.Label timerLabel;
 
-        /// <summary>
-        ///  Clean up any resources being used.
-        /// </summary>
-        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
         protected override void Dispose(bool disposing)
         {
             if (disposing && (components != null))
@@ -20,27 +15,39 @@
             base.Dispose(disposing);
         }
 
-        #region Windows Form Designer generated code
-
-        /// <summary>
-        ///  Required method for Designer support - do not modify
-        ///  the contents of this method with the code editor.
-        /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
+            this.timer = new System.Windows.Forms.Timer(this.components);
+            this.timerLabel = new System.Windows.Forms.Label();
             SuspendLayout();
+            // 
+            // timer
+            // 
+            this.timer.Interval = 1000; // 1 segundo en milisegundos
+            this.timer.Tick += new System.EventHandler(this.Timer_Tick);
+            // 
+            // timerLabel
+            // 
+            this.timerLabel.AutoSize = true;
+            this.timerLabel.Font = new Font("Arial", 24F, FontStyle.Bold, GraphicsUnit.Point);
+            this.timerLabel.Location = new Point(10, 10);
+            this.timerLabel.Name = "timerLabel";
+            this.timerLabel.Size = new Size(200, 37);
+            this.timerLabel.TabIndex = 0;
+            this.timerLabel.Text = "Tiempo: 60";
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(800, 450);
+            Controls.Add(this.timerLabel);
             Name = "Form1";
             Text = "Form1";
             Load += Form1_Load;
             ResumeLayout(false);
+            PerformLayout();
         }
-
-        #endregion
     }
 }
--- AirWar/Form1.cs
+++ AirWar/Form1.cs
@@ -1,19 +1,39 @@
+using System;
+using System.Windows.Forms;
+
 namespace AirWar
 {
     public partial class Form1 : Form
     {
+        private int timeLeft = 60; // Tiempo en segundos
+
         public Form1()
         {
-            this.Text = "AirWar";
             InitializeComponent();
             this.Text = "AirWar";
             this.WindowState = FormWindowState.Maximized; // Pantalla completa
             this.FormBorderStyle = FormBorderStyle.FixedDialog; // No se puede cambiar el tamaño
+            this.MaximizeBox = false; // Deshabilitar el botón de maximizar
+            this.MinimizeBox = false; // Deshabilitar el botón de minimizar
         }
 
         private void Form1_Load(object sender, EventArgs e)
         {
+            timer.Start(); // Iniciar el timer cuando se carga el formulario
+        }
 
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            if (timeLeft > 0)
+            {
+                timeLeft--; // Decrementar el tiempo
+                timerLabel.Text = $"Tiempo: {timeLeft}"; // Actualizar el texto del label
+            }
+            else
+            {
+                timer.Stop(); // Detener el timer
+                MessageBox.Show("Tu puntuación fue de: ", "Tiempo terminado", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
         }
     }
 }
